c48ddd8004e269d4c743b680f2038a94e8eff817
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-24 15:27:30 +0200

    Update and rename install-protonvpn.txt to install-protonvpn.sh

--- jellyfin/install-protonvpn.sh
+++ jellyfin/install-protonvpn.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
 wget -o "/home/server/server-setup-files/protonvpn.deb" "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-2_all.deb"
 
-# bash <(curl -s http://mywebsite.example/myscript.txt)
+# execute the script from terminal:
+# wget -O - http://mywebsite.example/myscript.txt | bash
