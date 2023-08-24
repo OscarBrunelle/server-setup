25d9ce5a5305249c9221055759e2a3c746c81223
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-24 17:02:38 +0200

    Update install-protonvpn.sh

--- jellyfin/install-protonvpn.sh
+++ jellyfin/install-protonvpn.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+app_name="Proton VPN"
+echo "Installing '${app_name}'"
+
 wget -o "/home/server/server-setup-files/protonvpn.deb" "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-2_all.deb"
 
-# execute the script from terminal:
-# wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-protonvpn.sh" | bash
+echo "Successfully installed '${app_name}'"
