b41251f3e20255df4a388b8448cb344da51282e3
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-24 17:11:27 +0200

    Update install-protonvpn.sh

--- jellyfin/install-protonvpn.sh
+++ jellyfin/install-protonvpn.sh
@@ -4,5 +4,7 @@ app_name="Proton VPN"
 echo "Installing '${app_name}'"
 
 wget -o "/home/server/server-setup-files/protonvpn.deb" "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-2_all.deb"
+sudo apt-get install protonvpn
+sudo apt install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1
 
 echo "Successfully installed '${app_name}'"
