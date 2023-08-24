b1f1f574219a452122ff67f6c9b3483c4bef662c
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-25 00:34:29 +0200

    Update install-all.sh

--- jellyfin/install-all.sh
+++ jellyfin/install-all.sh
@@ -4,6 +4,10 @@ sudo apt update
 sudo apt full-upgrade
 
 wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-protonvpn.sh" | bash
+wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-qbittorrent.sh" | bash
+wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-jellyfin.sh" | bash
+wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-sonarr.sh" | bash
+wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-jackett.sh" | bash
 
 # execute this script from the terminal:
 # wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-all.sh" | bash
