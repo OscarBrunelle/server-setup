07bbe0e3920d4a12cfbd7fa07c9c5d29952261df
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-24 17:20:02 +0200

    Create install-qbittorrent.sh

---
+++ jellyfin/install-qbittorrent.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Link: https://doc.ubuntu-fr.org/qbittorrent
+
+app_name="QBitTorrent"
+echo "Installing '${app_name}'"
+
+sudo apt install qbittorrent
+
+echo "Successfully installed '${app_name}'"
