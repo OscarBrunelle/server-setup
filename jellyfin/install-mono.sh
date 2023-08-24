860f89da70cf578b87bf1a162f6b6a8ab8779a49
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-24 20:15:56 +0200

    Create install-mono.sh

---
+++ jellyfin/install-mono.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Link: https://www.mono-project.com/download/stable/#download-lin-debian
+# Mono needed for Sonarr
+
+app_name="Mono"
+echo "Installing '${app_name}'"
+
+sudo apt install dirmngr ca-certificates gnupg
+sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/debian stable-buster main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono-devel
+
+echo "Successfully installed '${app_name}'"
