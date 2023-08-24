f5970c69b0e129b4ac554051b0b3841c39d2b630
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-24 17:25:46 +0200

    Create install-jellyfin.sh

---
+++ jellyfin/install-jellyfin.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Documentation: https://doc.ubuntu-fr.org/jellyfin
+
+app_name="Jellyfin"
+echo "Installing '${app_name}'"
+
+sudo apt install apt-transport-https
+sudo add-apt-repository universe
+wget -O - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | sudo apt-key add -
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/ubuntu $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+sudo apt update
+sudo apt install jellyfin
+
+echo "Successfully installed '${app_name}'"
