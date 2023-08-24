289b7ba0fe1fed3d2c41cbcff80d60b2fd45507e
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-24 16:14:11 +0200

    Update install-all.sh

--- jellyfin/install-all.sh
+++ jellyfin/install-all.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
 sudo apt update
 sudo apt full-upgrade
 
 wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-protonvpn.sh" | bash
+
+# execute this script from the terminal:
+# wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-all.sh" | bash
