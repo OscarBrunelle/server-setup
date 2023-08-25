afb4298df344b86c326625f9b3e9fcdeedb0173e
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-25 10:27:23 +0200

    Update install-mediainfo.sh

--- jellyfin/install-mediainfo.sh
+++ jellyfin/install-mediainfo.sh
@@ -2,8 +2,12 @@
 # MediaInfo needed for Sonarr
 
 app_name="MediaInfo"
+echo "##########"
 echo "Installing '${app_name}'"
+echo "##########"
 
 sudo wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-21_all.deb && sudo dpkg -i repo-mediaarea_1.0-21_all.deb && sudo apt-get update
 
+echo "##########"
 echo "Successfully installed '${app_name}'"
+echo "##########"
