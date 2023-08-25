1a510d57137013bbc690d70330ee81a22417d28d
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-25 11:00:38 +0200

    Update install-all.sh

--- jellyfin/install-all.sh
+++ jellyfin/install-all.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo "##########"
+echo "Installing Jellyfin server's needed components"
+echo "##########"
+
 sudo apt update
 sudo apt full-upgrade
+sudo apt install net-tools
 
 wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-protonvpn.sh" | bash
 wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-qbittorrent.sh" | bash
@@ -9,5 +14,10 @@ wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/insta
 wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-sonarr.sh" | bash
 wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-jackett.sh" | bash
 
+ifconfig
+
+echo "##########"
+echo "Successfully installed all Jellyfin server's needed components !"
+echo "##########"
 # execute this script from the terminal:
 # wget -O - "https://github.com/OscarBrunelle/server-setup/raw/main/jellyfin/install-all.sh" | bash
