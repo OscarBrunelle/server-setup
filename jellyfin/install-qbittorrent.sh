764faf777f6767bfaf74fa979ae8f5ab14755750
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-25 10:28:15 +0200

    Update install-qbittorrent.sh

--- jellyfin/install-qbittorrent.sh
+++ jellyfin/install-qbittorrent.sh
@@ -2,8 +2,12 @@
 # Link: https://doc.ubuntu-fr.org/qbittorrent
 
 app_name="QBitTorrent"
+echo "##########"
 echo "Installing '${app_name}'"
+echo "##########"
 
 sudo apt install qbittorrent
 
+echo "##########"
 echo "Successfully installed '${app_name}'"
+echo "##########"
