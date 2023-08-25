f1aa90ada4be1d54b65cee8f8253d8b1b8f0073d
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-25 10:25:44 +0200

    Update configure-jackett.sh

--- jellyfin/configure-jackett.sh
+++ jellyfin/configure-jackett.sh
@@ -2,10 +2,14 @@
 # Web server url: http://localhost:9117/
 
 app_name="Jackett"
+echo "##########"
 echo "Configuring '${app_name}'"
+echo "##########"
 
 # todo
 # Jackett indexers: http://192.168.x.y:9117/UI/dashboard
 # 1337x Anime Tosho SubsPlease The Pirate Bay
 
+echo "##########"
 echo "Successfully configured '${app_name}'"
+echo "##########"
