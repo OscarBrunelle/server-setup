a22899d20ce74237b55b360fc1ce9ee6c711c39c
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-25 10:26:30 +0200

    Update configure-sonarr.sh

--- jellyfin/configure-sonarr.sh
+++ jellyfin/configure-sonarr.sh
@@ -2,7 +2,9 @@
 # Web server url: http://localhost:8989
 
 app_name="Sonarr"
+echo "##########"
 echo "Configuring '${app_name}'"
+echo "##########"
 
 # If can't select dl folder
 sudo chown -R sonarr:sonarr /media
@@ -10,4 +12,6 @@ sudo chown -R sonarr:sonarr /media
 # If needed to start / stop the service
 # sudo systemctl start sonarr.service
 
+echo "##########"
 echo "Successfully configured '${app_name}'"
+echo "##########"
