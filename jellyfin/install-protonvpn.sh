880400a33603c312420b6cbce473e70759d2a632
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-25 11:49:50 +0200

    Update install-protonvpn.sh

--- jellyfin/install-protonvpn.sh
+++ jellyfin/install-protonvpn.sh
@@ -6,11 +6,11 @@ echo "##########"
 echo "Installing '${app_name}'"
 echo "##########"
 
-wget "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-2_all.deb"
+wget -o "/home/server/server-setup-files/protonvpn.deb" "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-2_all.deb"
 # To check the checksum:
 # echo "c68a0b8dad58ab75080eed7cb989e5634fc88fca051703139c025352a6ee19ad  protonvpn-stable-release_1.0.3-2_all.deb" | sha256sum --check -
-sudo apt-get install "protonvpn-stable-release_1.0.3-2_all.deb"
-rm "protonvpn-stable-release_1.0.3-2_all.deb"
+sudo apt-get install "/home/server/server-setup-files/protonvpn.deb"
+rm "/home/server/server-setup-files/protonvpn.deb"
 
 sudo apt-get install protonvpn-cli
 # sudo apt install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1
