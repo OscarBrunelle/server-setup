e9b97446ed499b9e3214d2d7c6b8e2ffa78330a8
Oscar Brunelle <oscar.brunelle@gmail.com>
2023-08-26 14:37:31 +0200

    Update install-protonvpn.sh

--- jellyfin/install-protonvpn.sh
+++ jellyfin/install-protonvpn.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
-# Link: https://protonvpn.com/support/linux-ubuntu-vpn-setup/
+# Link: https://github.com/Rafficer/linux-cli-community
 
 app_name="Proton VPN"
 echo "##########"
 echo "Installing '${app_name}'"
 echo "##########"
 
-sudo wget "https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb"
-# To check the checksum:
-# echo "c68a0b8dad58ab75080eed7cb989e5634fc88fca051703139c025352a6ee19ad  protonvpn-stable-release_1.0.3_all.deb" | sha256sum --check -
-sudo apt-get install "/home/server/protonvpn-stable-release_1.0.3_all.deb"
-sudo rm "/home/server/protonvpn-stable-release_1.0.3_all.deb*"
-
-sudo apt-get update
-sudo apt-get install protonvpn-cli
-# sudo apt install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1
+sudo apt install -y openvpn dialog python3-pip python3-setuptools
+sudo pip3 install protonvpn-cli
+sudo protonvpn init
+sudo protonvpn connect
 
 echo "##########"
 echo "Successfully installed '${app_name}'"
